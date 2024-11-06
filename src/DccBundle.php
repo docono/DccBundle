fdf93b62c7b082b8ee98c67def888eaa71f41c9a
Renzo <renzo@docono.io>
2024-11-06 14:19:19 +0100

    adjustments for Pimcore11

--- src/DccBundle.php
+++ src/DccBundle.php
@@ -2,23 +2,24 @@
 
 namespace docono\Bundle\DccBundle;
 
+
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\PimcoreBundleAdminClassicInterface;
+use Pimcore\Extension\Bundle\Traits\BundleAdminClassicTrait;
 
-class DccBundle extends AbstractPimcoreBundle
+class DccBundle extends AbstractPimcoreBundle implements PimcoreBundleAdminClassicInterface
 {
-    public function __construct()
-    {
+    use BundleAdminClassicTrait;
 
-    }
-
-    public function getJsPaths()
+    public function getJsPaths(): array
     {
         return [
 
         ];
     }
 
-    public function getCssPaths() {
+    public function getCssPaths(): array
+    {
         return [
 
         ];
